3f714b786ddd3f4f890c193f1836e65b74fc38ff
Jordan Poste <jposte@gmail.com>
2016-11-22 00:56:05 +0000

    added some login shinanigans - Nov 21

--- app/actions.rb
+++ app/actions.rb
@@ -1,10 +1,70 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @posts = Post.order(created_at: :desc)
+  # Removed this because replaced it with helper method do didn't have to repeat it multiple times across all 
+  # requests @current_user = User.find_by(id: session[:user_id]) #reuired so we can display username at top of screen (see html)
   erb(:index)   
 end
-    
-    
-    
+
+get '/signup' do      # if a user nagigates to the path "/signup",
+  @user = User.new    # setup empty @user object
+  erb (:signup)       # render "app/views/signup.erb"
+end
+
+post '/signup' do
+  
+  # grab user input values from params
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+  
+  # instantiate a User
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+  
+  #if user validations pass and user is saved
+  if @user.save
+    redirect to('/login') #will trigger the get '/login' action and thus setting @user = User.new and rendering out login.erb
+  else
+    erb(:signup)
+  end
+  
+end
+  
+get '/login' do # when a GET request comes into /login
+  erb(:login)   # render app/views/login.erb
+end
+ 
+post '/login' do  
+    #params.to_s    # just display the params for now to make sure it's working 
+  username = params[:username]
+  password = params[:password]
+  
+  # 1. find user by username
+  user = User.find_by(username: username)
+  
+  if user && user.password == password
+      session[:user_id] = user.id
+      redirect to ('/') # will trigger get '/' in actions.rb setting @posts and rendering index.erb (note: requests go to paths not files)
+      # removed this because want to be redirected to home page instead of message displayed...  "Success! User with id #{session[:user_id]} is logged in!"
+    else
+      @error_message = "Login failed."
+      erb(:login)
+    end
+
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to ('/')
+end
+  
+  
 =begin OLD CODE TO DELETE BECAUSE OF ACTIVE RECORD SETUP
     @post_shark = {
         username: "sharky_j",
